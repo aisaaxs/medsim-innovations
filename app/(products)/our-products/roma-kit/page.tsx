5de780f43020068d3e66178cfdf0d21190dfc2d5
Anitej Isaac Sharma <anitejsharmas@gmail.com>
2024-12-28 02:04:34 -0800

    product pages back button placement

--- app/(products)/our-products/roma-kit/page.tsx
+++ app/(products)/our-products/roma-kit/page.tsx
@@ -3,8 +3,13 @@
 import { motion } from "framer-motion";
 import RomaKitImg from "../../../../images/roma-kit.png";
 import Image from "next/image";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 export default function RomaKit() {
+    const router = useRouter();
+    
   return (
     <div className="w-full">
       <div className="relative h-auto py-10 bg-gradient-to-b from-green-400 to-white flex flex-col items-center justify-center text-center px-6">
@@ -24,6 +29,11 @@ export default function RomaKit() {
         >
           Learn and practice administering medications through various routes with the ROMA Kit.
         </motion.p>
+      </div>
+
+      <div className="border-2 border-gray-900 rounded-full w-auto max-w-36 h-auto px-4 py-2 flex justify-center items-center cursor-pointer relative left-1/2 transform -translate-x-1/2 hover:bg-gray-900 z-50 hover:text-white group transition-all duration-300 flex-row gap-x-2" onClick={() => {router.push('/our-products')}}>
+          <FontAwesomeIcon icon={faArrowLeft} className="group-hover:text-white text-gray-900 transition-all duration-300" />
+          <p className="font-bold">Back</p>
       </div>
 
       <div className="relative bg-white py-16 px-6 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
